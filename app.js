8c3c384ff6d56aff768a5c251053648461c907c3
Musala Ndouvhada <2686346@students.wits.ac.za>
2025-07-15 14:49:16 +0200

    First commit

--- app.js
+++ app.js
@@ -16,21 +16,36 @@ app.get('/', (req, res) => {
 });
 
 // Socket.IO connection handler
+// In app.js, modify the connection handler
 io.on('connection', (socket) => {
-  console.log('A user connected');
-
-  // Handle new messages
-  socket.on('chat message', (msg) => {
-    console.log('Message received:', msg);
-    // Broadcast the message to all connected clients
-      io.emit('chat message', msg);
+    console.log('A user connected');
+  
+    // Store username with socket
+    socket.username = 'Anonymous';
+    // Handle new messages with username
+    socket.on('chat message', (msg) => {
+      io.emit('chat message', {
+        username: socket.username,
+        message: msg,
+        timestamp: new Date().toISOString()
+      });
     });
-
-  // Handle disconnection
-  socket.on('disconnect', () => {
-    console.log('A user disconnected');
+    // Handle username change
+    socket.on('set username', (username) => {
+      const oldUsername = socket.username;
+      socket.username = username || 'Anonymous';
+      io.emit('user joined', {
+        oldUsername: oldUsername,
+        newUsername: socket.username
+      });
+    });
+  
+    // Handle disconnection
+    socket.on('disconnect', () => {
+      console.log('A user disconnected');
+      io.emit('user left', { username: socket.username });
+    });
   });
-});
 
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
